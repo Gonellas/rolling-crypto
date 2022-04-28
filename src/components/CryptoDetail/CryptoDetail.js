1057de039fc3309086733e3f5872d9ff0e20e48f
Pablo Marino <pmarino2013@gmail.com>
2022-04-28 15:25:01 -0300

    Arreglando peticiones para traer detalles de tickers

--- src/components/CryptoDetail/CryptoDetail.js
+++ src/components/CryptoDetail/CryptoDetail.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import "./CryptoDetail.css";
 
 const CryptoDetail = ({ contract_name }) => {
+  const params = useParams();
   const [datos, setDatos] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(
-      "https://api.covalenthq.com/v1/pricing/volatility/?quote-currency=USD&format=JSON&tickers&key=ckey_1c4049de4f7342d691c6d5eb431"
+      `https://api.covalenthq.com/v1/pricing/volatility/?quote-currency=USD&format=JSON&tickers=${params.id}&key=ckey_1c4049de4f7342d691c6d5eb431`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -19,54 +21,57 @@ const CryptoDetail = ({ contract_name }) => {
         );
         setDatos(cryptoData);
       });
-  }, []);
+  }, [params.id]);
 
   return (
     <div className="detail-style">
       <Container fluid className="coin-container">
-        {loading
-          ? "Cargando"
-          : datos.map((element, index) => {
-              return (
-                <>
-                  <Row className="row1">
-                    <Col key={index} className="coin-name">
-                      {element.contract_ticker_symbol} - {element.contract_name}
-                    </Col>
-                    <Col>
-                      <img
-                        className="img-style"
-                        src={element.logo_url}
-                        alt={element.contract_name}
-                      />
-                    </Col>
-                    <h2 className="price">PRICE: {element.quote_rate} USD</h2>
-                    <Col>
-                    </Col>
-                  </Row>
-                  <Row className="row2">
-                    <Col className="pv-style">
-                      <h2>PRICE VOLATILITY</h2>
-                    </Col>
-                    <Col>
-                      <h3>2 Hours: {element.stddev_2h}</h3>
-                    </Col>
-                    <Col>
-                      <h3>4 Hours: {element.stddev_4h}</h3>
-                    </Col>
-                    <Col>
-                      <h3>8 Hours: {element.stddev_8h}</h3>
-                    </Col>
-                    <Col>
-                      <h3>16 Hours: {element.stddev_16h}</h3>
-                    </Col>
-                    <Col>
-                      <h3>24 Hours: {element.stddev_24h}</h3>
-                    </Col>
-                  </Row>
-                </>
-              );
-            })}
+        {loading ? (
+          "Cargando"
+        ) : datos.length === 0 ? (
+          <h3>No hay información</h3>
+        ) : (
+          datos.map((element, index) => {
+            return (
+              <>
+                <Row className="row1">
+                  <Col key={index} className="coin-name">
+                    {element.contract_ticker_symbol} - {element.contract_name}
+                  </Col>
+                  <Col>
+                    <img
+                      className="img-style"
+                      src={element.logo_url}
+                      alt={element.contract_name}
+                    />
+                  </Col>
+                  <h2 className="price">PRICE: {element.quote_rate} USD</h2>
+                  <Col></Col>
+                </Row>
+                <Row className="row2">
+                  <Col className="pv-style">
+                    <h2>PRICE VOLATILITY</h2>
+                  </Col>
+                  <Col>
+                    <h3>2 Hours: {element.stddev_2h}</h3>
+                  </Col>
+                  <Col>
+                    <h3>4 Hours: {element.stddev_4h}</h3>
+                  </Col>
+                  <Col>
+                    <h3>8 Hours: {element.stddev_8h}</h3>
+                  </Col>
+                  <Col>
+                    <h3>16 Hours: {element.stddev_16h}</h3>
+                  </Col>
+                  <Col>
+                    <h3>24 Hours: {element.stddev_24h}</h3>
+                  </Col>
+                </Row>
+              </>
+            );
+          })
+        )}
       </Container>
     </div>
   );
--- src/components/HomePage/SectionTwo/SectionTwo.js
+++ src/components/HomePage/SectionTwo/SectionTwo.js
@@ -2,27 +2,26 @@ import { ListGroup } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const SectionTwo = () => {
-
   const [datos, setDatos] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(
-      "https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=USD&format=JSON&key=ckey_1c4049de4f7342d691c6d5eb431"
+      "https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=USD&format=JSON&key=ckey_1c4049de4f7342d691c6d5eb431&page-size=20&page-number=0"
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.data.items);
         setLoading(false);
-        const topTen = data.data.items.filter((element) => element.rank <= 25);
+        const topTen = data.data.items.filter(
+          (element) => element.contract_name !== null
+        );
         setDatos(topTen);
       });
   }, []);
 
   return (
-
     <section className="st-style">
       <>
         {loading
@@ -30,18 +29,21 @@ const SectionTwo = () => {
           : datos.map((element, index) => {
               return (
                 <>
-                 <ListGroup className="list-group">
-                    <ListGroup.Item                    
-                      className="list-link d-flex justify-content-between align-items-start"
-                    >
+                  <ListGroup className="list-group" key={index}>
+                    <ListGroup.Item className="list-link d-flex justify-content-between align-items-start">
                       <div className=" list-container">
-                        <Link to={`/detail/${element.contract_name}`} className="list-link cts">{element.contract_ticker_symbol}</Link>
-                        <br />                        
+                        <Link
+                          to={`/detail/${element.contract_ticker_symbol}`}
+                          className="list-link cts"
+                        >
+                          {element.contract_ticker_symbol}
+                        </Link>
+                        <br />
                         {element.contract_name}
                         <hr />
-                      </div>                    
+                      </div>
                     </ListGroup.Item>
-              </ListGroup>
+                  </ListGroup>
                 </>
               );
             })}
